fc88de8146fccb022ed32ebdd0f26ada220725d6
DeShyam01 <shyamsundargayen345@gmail.com>
2025-12-21 02:15:05 +0530

    added auth middleware for using tasks

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        const {token} = req.headers
+        if(!token){
+            return res.status(401).json({message: "Unauthorized"})
+        }
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+        if(!decodedToken){
+            return res.status(401).json({message: "Invalid Token"})
+        }
+        req.user = decodedToken
+        next()
+    } catch (error) {
+        console.log("Error in authMiddleware: ", error)
+        next(error)
+    }
+}
+
+module.exports = authMiddleware
