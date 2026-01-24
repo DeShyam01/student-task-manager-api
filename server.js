c26fb22fed16f0c213b5a1a1e25f91e56212d87d
DeShyam01 <shyamsundargayen345@gmail.com>
2026-01-24 18:03:30 +0530

    test

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
-    credentials: true,
-  }
-));
+app.use(cors());
 app.use(requestLogger);
 app.use(globalErrorCatcher);
 
